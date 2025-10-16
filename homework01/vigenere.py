7974075d6a1deb2ba8ba736a06319edce24f61c6
Aleksei <kexfff@yandex.ru>
2025-10-16 14:13:07 +0300

    Vigenere encryption

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -8,8 +8,47 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
+
+    if len(keyword)<len(plaintext):
+        keyword=keyword*(len(plaintext)//len(keyword)+1)
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    low="abcdefghijklmnopqrstuvwxyz"
+    up=low.upper()
+    i=0
+    while i<len(plaintext):
+        c=plaintext[i]
+        shift=low.find(keyword.lower()[i])
+        if c.isdigit():
+            ciphertext+=c
+            i+=1
+            continue
+        if c.isspace():
+            ciphertext+=c
+            i+=1
+            continue
+        if c.isalpha():
+            if c.isupper():
+                if (up.find(c) + shift)>=len(up):
+                    a=(up.find(c)+shift)%len(up)
+                else:
+                    a=up.find(c)+shift
+                b=up[a]
+            else:
+                if (low.find(c) + shift)>=len(up):
+                    a=(low.find(c)+shift)%len(up)
+                else:
+                    a=low.find(c)+shift
+                b=low[a]
+
+            ciphertext+=b
+            i+=1
+
+            #a=ord(c)+shift
+            #ciphertext+=chr(a)
+            continue
+        ciphertext+=c
+        i+=1
+        
     return ciphertext
 
 
@@ -23,6 +62,47 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
+
+    if len(keyword)<len(ciphertext):
+        keyword=keyword*(len(ciphertext)//len(keyword)+1)
     plaintext = ""
-    # PUT YOUR CODE HERE
+    low="abcdefghijklmnopqrstuvwxyz"
+    up=low.upper()
+    i=0
+    while i<len(ciphertext):
+        c=ciphertext[i]
+        shift=low.find(keyword.lower()[i])
+        if c.isdigit():
+            plaintext+=c
+            i+=1
+            continue
+        if c.isspace():
+            plaintext+=c
+            i+=1
+            continue
+        if c.isalpha():
+            if c.isupper():
+                if (up.find(c) - shift)<0:
+                    a=(up.find(c)-shift)%len(up)
+                else:
+                    a=up.find(c)-shift
+                b=up[a]
+            else:
+                if (low.find(c) - shift)<0:
+                    a=(low.find(c)-shift)%len(up)
+                else:
+                    a=low.find(c)-shift
+                b=low[a]
+
+            plaintext+=b
+            i+=1
+
+            #a=ord(c)+shift
+            #ciphertext+=chr(a)
+            continue
+        plaintext+=c
+        i+=1
     return plaintext
+
+print(encrypt_vigenere("ATTACKATDAWN","LEMONLEMONLE"))
+print(decrypt_vigenere("PYTHON","A"))
