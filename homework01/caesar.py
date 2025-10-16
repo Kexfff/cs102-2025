c6a1e1f46116408de1f5f341d80fe8bd1373c8ff
Aleksei <kexfff@yandex.ru>
2025-10-16 13:35:54 +0300

    ceasar encryption

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -10,8 +10,38 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     >>> encrypt_caesar("")
     ''
     """
+    low="abcdefghijklmnopqrstuvwxyz"
+    up=low.upper()
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    if shift==0:
+        return plaintext
+    for c in plaintext:
+        if c.isdigit():
+            ciphertext+=c
+            continue
+        if c.isspace():
+            ciphertext+=c
+            continue
+        if c.isalpha():
+            if c.isupper():
+                if (up.find(c) + shift)>len(up):
+                    a=up.find(c)+shift-len(up)
+                else:
+                    a=up.find(c)+shift
+                b=up[a]
+            else:
+                if (low.find(c) + shift)>len(up):
+                    a=low.find(c)+shift-len(up)
+                else:
+                    a=low.find(c)+shift
+                b=low[a]
+
+            ciphertext+=b
+
+            #a=ord(c)+shift
+            #ciphertext+=chr(a)
+            continue
+        ciphertext+=c
     return ciphertext
 
 
@@ -27,6 +57,45 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     >>> decrypt_caesar("")
     ''
     """
+
+    low="abcdefghijklmnopqrstuvwxyz"
+    up=low.upper()
     plaintext = ""
-    # PUT YOUR CODE HERE
+    if shift==0:
+        return ciphertext
+    for c in ciphertext:
+        if c.isdigit():
+            plaintext+=c
+            continue
+        if c.isspace():
+            plaintext+=c
+            continue
+        if c.isalpha():
+            if c.isupper():
+                if (up.find(c) - shift)<0:
+                    a=up.find(c)-shift+len(up)
+                else:
+                    a=up.find(c)-shift
+                b=up[a]
+            else:
+                if (low.find(c) - shift)<0:
+                    a=low.find(c)-shift+len(up)
+                else:
+                    a=low.find(c)-shift
+                b=low[a]
+
+            plaintext+=b
+
+            #a=ord(c)+shift
+            #ciphertext+=chr(a)
+            continue
+        plaintext+=c
+    
+
     return plaintext
+
+
+
+
+print(encrypt_caesar("Python3.6",3))
+print(decrypt_caesar("Sbwkrq3.6",3))
